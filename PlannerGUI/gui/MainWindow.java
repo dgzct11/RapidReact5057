311b623d91c55346f568db965cb371dc7ad16636
Ryan Bohluli <ryan.bohluli@gmail.com>
2022-01-22 09:10:42 -0600

    Added previous GUI

---
+++ PlannerGUI/gui/MainWindow.java
@@ -0,0 +1,35 @@
+package app.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+public class MainWindow {
+    private SubsystemControlPanel controlPanel;
+    private TrajectoryPlanning trajectoryPlanning;
+    private Velocity velocity;
+    public void display() {
+        trajectoryPlanning = new TrajectoryPlanning();
+        controlPanel = new SubsystemControlPanel();
+        velocity = new Velocity();
+       
+        trajectoryPlanning.setVelocity(velocity);
+        trajectoryPlanning.setControlPanel(controlPanel);
+        velocity.setTrajectory(trajectoryPlanning);
+        controlPanel.setTrajectory(trajectoryPlanning);
+        
+        trajectoryPlanning.display();
+        velocity.display();
+        controlPanel.display();
+    }
+}
