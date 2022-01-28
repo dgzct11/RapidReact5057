9da1a60a45371f856b665b86af0811f44624f8cd
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2022-01-28 13:38:48 -0600

    added jetson vision

--- Robot Code/src/main/java/frc/robot/subsystems/sensors/JetsonVision.java
+++ Robot Code/src/main/java/frc/robot/subsystems/sensors/JetsonVision.java
@@ -4,14 +4,38 @@
 
 package frc.robot.subsystems.sensors;
 
+import java.util.ArrayList;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class JetsonVision extends SubsystemBase {
   /** Creates a new JetsonVision. */
-  public JetsonVision() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+
+  ArrayList<NetworkTableEntry> ball_entries = new ArrayList<NetworkTableEntry>();
+  double[] zereos = {0,0,0,0,0,0, 0};
+  // vector = [x,y,width, height, confidence, red or blue ball, camera_number]
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("balldata");
+  
+  public JetsonVision() {
+    
+      for(int i = 0; i<22; i++){
+        ball_entries.add(table.getEntry("ball_"+(i+1)));
+        ball_entries.get(0).setDoubleArray(zereos);
+      }
+
   }
+
+  public double[] getAngleDifference(double[] ball){
+    return zereos;
+  }
+  public double getDistance(double[] ball_angle_diff){
+    return 0;
+  }
+
+
+  
 }
