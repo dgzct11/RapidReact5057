e4ca44e37fb6dd7747044454a6812ff178231704
Nelson Vo <54444005+nelsonvo1234@users.noreply.github.com>
2022-01-29 10:12:24 -0600

    Added compressor

--- Robot Code/src/main/java/frc/robot/subsystems/mechanical_subsystems/Intake.java
+++ Robot Code/src/main/java/frc/robot/subsystems/mechanical_subsystems/Intake.java
@@ -5,10 +5,21 @@
 package frc.robot.subsystems.mechanical_subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  public Intake() {}
+  
+  public Intake() {
+    Compressor comp = new Compressor(1, PneumaticsModuleType.REVPH);
+    DoubleSolenoid ds = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.forward_channel, Constants.reverse_channel);
+  }
   //TODO
   @Override
   public void periodic() {
