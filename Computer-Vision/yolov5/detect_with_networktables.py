9851ab8463966b2e70146f9a7db4670eb1e2174c
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2022-02-04 21:44:43 -0600

    Update detect_with_networktables.py

--- Computer-Vision/yolov5/detect_with_networktables.py
+++ Computer-Vision/yolov5/detect_with_networktables.py
@@ -23,8 +23,8 @@ Usage - formats:
                                          yolov5s.tflite             # TensorFlow Lite
                                          yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
 """
-from networktables import NetworkTables
-print(f'\n\n\nconnected: {NetworkTables.initialize(server="10.50.57.2")} ')
+#from networktables import NetworkTables
+#print(f'\n\n\nconnected: {NetworkTables.initialize(server="10.50.57.2")} ')
 
 import argparse
 import os
@@ -50,6 +50,18 @@ from utils.torch_utils import select_device, time_sync
 
 
 @torch.no_grad()
+def updateNetwork(pred):
+    conf_thres = 0.7
+    balls = NetworkTables.getTable('balldata')
+    ball_num = 1
+    for i, det in enumerate(pred):
+      for tens in det:
+        if(tens[4] > conf_thres):
+          balls.putNumberArray(f"ball_{ball_num}", [tens[0], tens[1], tens[2], tens[3], tens[4], tens[5]])
+          ball_num+=1
+      while ball_num<22:
+          balls.putNumberArray(f"ball_{ball_num}", [0,0,0,0,0])
+          ball_num+=1
 def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
@@ -78,16 +90,12 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         dnn=False,  # use OpenCV DNN for ONNX inference
         ):
     source = str(source)
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
-    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
-    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
-    webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file)
-    if is_url and is_file:
-        source = check_file(source)  # download
+    
+    
+    webcam = True
+    
 
-    # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+   
 
     # Load model
     device = select_device(device)
@@ -100,15 +108,14 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
     if pt or jit:
         model.model.half() if half else model.model.float()
 
-    # Dataloader
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
-        bs = len(dataset)  # batch_size
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
-        bs = 1  # batch_size
+    # Dataloader Webcam
+  
+   
+    cudnn.benchmark = True  # set True to speed up constant image size inference
+    #source = ["0", "1"]
+    dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
+    bs = len(dataset)  # batch_size
+  
     vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
@@ -125,7 +132,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         dt[0] += t2 - t1
 
         # Inference
-        visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+        
         pred = model(im, augment=augment, visualize=visualize)
         t3 = time_sync()
         dt[1] += t3 - t2
@@ -156,30 +163,17 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         cx, cy, w, h, conf, pred_cls(80)
 
         """
-        conf_thres = 0.7
-        balls = NetworkTables.getTable('balldata')
-        ball_num = 1
-        for i, det in enumerate(pred):
-          for tens in det:
-            if(tens[4] > conf_thres):
-              balls.putNumberArray(f"ball_{ball_num}", [tens[0], tens[1], tens[2], tens[3], tens[4], tens[5]])
-              ball_num+=1
-          while ball_num<22:
-              balls.putNumberArray(f"ball_{ball_num}", [0,0,0,0,0])
-              ball_num+=1
+        #updateNetwork(pred)
           
 
         for i, det in enumerate(pred):  # per image
             seen += 1
-            if webcam:  # batch_size >= 1
-                p, im0, frame = path[i], im0s[i].copy(), dataset.count
-                s += f'{i}: '
-            else:
-                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+            
+            p, im0, frame = path[i], im0s[i].copy(), dataset.count
+            s += f'{i}: '
+           
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+           
             s += '%gx%g ' % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
@@ -194,54 +188,22 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                    if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        annotator.box_label(xyxy, label, color=colors(c, True))
-                        if save_crop:
-                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+              
+                    
+                   
 
             # Print time (inference-only)
             LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
 
             # Stream results
-            im0 = annotator.result()
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
+            
 
             # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                            save_path += '.mp4'
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer[i].write(im0)
-
+           
     # Print results
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+  
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
 
