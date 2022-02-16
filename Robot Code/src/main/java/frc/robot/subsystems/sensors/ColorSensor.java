cefa25c8b3268c6b3110ddb101d3a82a8463f15b
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2022-02-16 09:13:48 -0600

    added code for spark max

--- Robot Code/src/main/java/frc/robot/subsystems/sensors/ColorSensor.java
+++ Robot Code/src/main/java/frc/robot/subsystems/sensors/ColorSensor.java
@@ -9,7 +9,7 @@
 package frc.robot.subsystems.sensors;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.revrobotics.ColorSensorV3;
 public class ColorSensor extends SubsystemBase {
   /** Creates a new ColorSensor. */
   public ColorSensor() {}
